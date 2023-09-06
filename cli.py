6595ceb62adff52fa52bf3aa815fd7cfe69d4f0d
kipngenohaaron <kipngenohaaron@gmail.com>
2023-09-06 22:47:37 +0300

    cli.py modified

---
+++ cli.py
@@ -0,0 +1,53 @@
+import click
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, AuthorGenre, Book, Genre, Customer, OrderItem
+from datetime import datetime
+
+# Replace 'your_database_url' with your actual database URL
+database_url = 'sqlite:///bookstore.db'
+
+engine = create_engine(database_url)
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+@click.group()
+def cli():
+    """Bookstore Management System CLI"""
+
+@cli.command()
+def list_books():
+    """List all books in the bookstore"""
+    session = DBSession()
+    books = session.query(Book).all()
+    session.close()
+    for book in books:
+        print(f"Book ID: {book.book_id}, Title: {book.title}, Author: {book.author_genre.author_name}, Genre: {book.genre.genre_name}")
+
+@cli.command()
+@click.argument('title')
+@click.argument('author')
+@click.argument('genre')
+@click.argument('publication_year', type=int)
+@click.argument('price', type=float)
+def add_book(title, author, genre, publication_year, price):
+    """Add a new book to the bookstore"""
+    session = DBSession()
+    
+    # Check if author and genre already exist in the database
+    author_genre = session.query(AuthorGenre).filter_by(author_name=author, genre_name=genre).first()
+    if author_genre is None:
+        author_genre = AuthorGenre(author_name=author, genre_name=genre)
+        session.add(author_genre)
+        session.commit()
+    
+    book = Book(title=title, author_genre=author_genre, publication_year=publication_year, price=price)
+    session.add(book)
+    session.commit()
+    
+    session.close()
+    print(f"Book '{title}' added successfully!")
+
+if __name__ == '__main__':
+    cli()
