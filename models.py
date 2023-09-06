36d33ef59376229bd46a8c57cc966c763fd033fb
kipngenohaaron <kipngenohaaron@gmail.com>
2023-09-06 22:44:44 +0300

    initial commit

---
+++ models.py
@@ -0,0 +1,77 @@
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class AuthorGenre(Base):
+    __tablename__ = 'author_genre'
+    
+    id = Column(Integer, primary_key=True)
+    author_name = Column(String)
+    birth_year = Column(Integer)
+    nationality = Column(String)
+    genre_name = Column(String)
+
+    # Define a one-to-many relationship with books
+    books = relationship('Book', back_populates='author')
+
+class Book(Base):
+    __tablename__ = 'book'
+    
+    book_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    
+    # Define foreign key relationships for author and genre
+    author_id = Column(Integer, ForeignKey('author_genre.id'))
+    genre_id = Column(Integer, ForeignKey('genre.id'))
+    
+    publication_year = Column(Integer)
+    price = Column(Float)
+
+    # Define many-to-one relationship with author_genre for author and genre
+    author = relationship('AuthorGenre', foreign_keys=[author_id], back_populates='books')
+    genre = relationship('Genre', foreign_keys=[genre_id], back_populates='books')
+
+    # Define one-to-many relationship with order_items
+    order_items = relationship('OrderItem', back_populates='book')  # This line should be included
+
+    # Rest of the class definition remains the same
+
+class Genre(Base):
+    __tablename__ = 'genre'
+
+    id = Column(Integer, primary_key=True)
+    genre_name = Column(String)
+
+    # Define a one-to-many relationship with books
+    books = relationship('Book', back_populates='genre')
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    
+    customer_id = Column(Integer, primary_key=True)
+    customer_name = Column(String)
+    email = Column(String)
+    phone = Column(String)
+
+    # Define a one-to-many relationship with order_items
+    order_items = relationship('OrderItem', back_populates='customer')
+
+class OrderItem(Base):
+    __tablename__ = 'order_item'
+    
+    order_id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customer.customer_id'))
+    book_id = Column(Integer, ForeignKey('book.book_id'))
+    order_date = Column(Date)
+    total_amount = Column(Float)
+    quantity_in_stock = Column(Integer)
+
+    # Define many-to-one relationships with customer and book
+    customer = relationship('Customer', back_populates='order_items')
+    book = relationship('Book', back_populates='order_items')
+
+    # Method to calculate the total order amount
+    def calculate_total_amount(self):
+        return self.total_amount
---
+++ seeds.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, AuthorGenre, Book, Genre, Customer, OrderItem
+from datetime import datetime
+
+# Replace 'your_database_url' with your actual database URL
+database_url = 'sqlite:///bookstore.db'
+
+engine = create_engine(database_url)
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Seed data for AuthorGenre
+authors = [
+    AuthorGenre(author_name='Author 1', birth_year=1980, nationality='Nationality 1', genre_name='Genre 1'),
+    AuthorGenre(author_name='Author 2', birth_year=1990, nationality='Nationality 2', genre_name='Genre 2')
+]
+
+session.add_all(authors)
+session.commit()
+
+# Seed data for Genre
+genres = [
+    Genre(genre_name='Genre 1'),
+    Genre(genre_name='Genre 2')
+]
+
+session.add_all(genres)
+session.commit()
+
+# Seed data for Book
+books = [
+    Book(title='Book 1', author_id=1, genre_id=1, publication_year=2000, price=20.0),
+    Book(title='Book 2', author_id=2, genre_id=2, publication_year=2010, price=25.0)
+]
+
+session.add_all(books)
+session.commit()
+
+# Seed data for Customer
+customers = [
+    Customer(customer_name='Customer 1', email='customer1@example.com', phone='123-456-7890'),
+    Customer(customer_name='Customer 2', email='customer2@example.com', phone='987-654-3210')
+]
+
+session.add_all(customers)
+session.commit()
+
+# Seed data for OrderItem
+order_items = [
+    OrderItem(customer_id=1, book_id=1, order_date=datetime.now(), total_amount=20.0, quantity_in_stock=10),
+    OrderItem(customer_id=2, book_id=2, order_date=datetime.now(), total_amount=25.0, quantity_in_stock=5)
+]
+
+session.add_all(order_items)
+session.commit()
+
+session.close()
