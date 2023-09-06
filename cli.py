d2fffbbf56f987815fba4b72e0886140733db3e5
kipngenohaaron <kipngenohaaron@gmail.com>
2023-09-06 23:36:15 +0300

    adding the authors

--- cli.py
+++ cli.py
@@ -16,15 +16,22 @@ DBSession = sessionmaker(bind=engine)
 def cli():
     """Bookstore Management System CLI"""
 
-# Command to list all books in the bookstore
 @cli.command()
 def list_books():
     """List all books in the bookstore"""
     session = DBSession()
     books = session.query(Book).all()
-    session.close()
     for book in books:
-        print(f"Book ID: {book.book_id}, Title: {book.title}, Author: {book.author.author_name}, Genre: {book.genre.genre_name}")
+        # Accessing related objects, so the session should remain open
+        author_name = book.author.author_name if book.author else "Unknown Author"
+        genre_name = book.genre.genre_name if book.genre else "Unknown Genre"
+        print(f"Book ID: {book.book_id}, Title: {book.title}, Author: {author_name}, Genre: {genre_name}")
+    
+    session.close()
+
+# Other commands...
+
+
 
 # Command to add a new book to the bookstore
 @cli.command()
@@ -41,12 +48,13 @@ def add_book(title, author, genre, publication_year, price):
     author_genre = session.query(AuthorGenre).filter_by(author_name=author, genre_name=genre).first()
     if author_genre is None:
         click.echo(f"Author '{author}' and Genre '{genre}' do not exist. Please add them first.")
+        session.close()
         return
     
     # Use the author_id and genre_id from the author_genre object
-    book = Book(title=title, author=author_genre, genre=author_genre.genre, publication_year=publication_year, price=price)
+    book = Book(title=title, author_id=author_genre.id, genre_id=author_genre.genre_id, publication_year=publication_year, price=price)
     session.add(book)
-    session.commit()
+    session.commit()  # Commit the changes to persist them in the database
     
     session.close()
     click.echo(f"Book '{title}' added successfully!")
